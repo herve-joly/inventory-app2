50d450404d3d3190564057e7c32c3afb15f9b391
= <=>
2023-08-20 21:30:09 +0100

    ref

--- server/server.js
+++ server/server.js
@@ -5,8 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 const init = async () => {
   try {
-    //await db.sync();
-
     app.listen(PORT, () => {
       console.log(`Server listening at http://localhost:${PORT}`);
     });
